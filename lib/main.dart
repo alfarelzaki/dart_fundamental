df61499b6c479579cde93120195f5f1aa057a521
alfarelzaki <alfarell.zaki@gmail.com>
2021-09-29 08:06:45 +0700

    text widget exploration

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,31 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0xffFECDCF),
+        backgroundColor: Color(0xffffffff),
         appBar: AppBar(
-          title: Text("MyApp")
+          title: Text("MyApp"),
+          backgroundColor: Colors.green,
         ),
         body: Center(
-          child: Text("This is homepage")
+          child: Text(
+            "This is homepage, i dont know what to write so i just write everything that cross my mind here",
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              backgroundColor: Colors.amber,
+              color: Colors.brown[800],
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 8,
+              fontFamily: "RobotoMono",
+              decorationStyle: TextDecorationStyle.wavy,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.green[400],
+              decorationThickness: 3
+
+            )
+          )
         ),
       )
     );
