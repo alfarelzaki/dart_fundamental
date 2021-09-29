013fbd5be955d933714d7d75ed3243a7078c38c3
alfarelzaki <alfarell.zaki@gmail.com>
2021-09-29 16:46:20 +0700

    list of text generator

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,30 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
-  
-  List<Color> colorList = [Colors.red, Colors.blue, Colors.green, Colors.amber];
+  final List<Color> colorList = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.amber
+  ];
+
+  final List<Widget> myList = List.generate(
+      100,
+      (index) => Text(
+            "${index + 1}",
+            style: TextStyle(fontSize: 20 + double.parse(index.toString())),
+          )
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("ListView")
-        ),
-
-        body: ListView.separated(
-          itemBuilder: (context, index) {
-            return Container(
-              height: 300,
-              width: 300,
-              color: colorList[index]
-            );
-          },
-          separatorBuilder: (context, index) {
-            return Divider();
-          },
-          itemCount: colorList.length),
+        appBar: AppBar(title: Text("ListView")),
+        body: ListView(
+          children: myList,
+        )
       ),
     );
   }
