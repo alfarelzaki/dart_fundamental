2102542f7fa126aca51aaf90f30a4d7ebf1f00ea
alfarelzaki <alfarell.zaki@gmail.com>
2021-10-02 22:30:04 +0700

    mapping list

--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,94 @@
 import 'package:faker/faker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int counter = 1;
+class MyApp extends StatelessWidget {
+  final List<Map<String, dynamic>> myList = [
+    {
+      "Name": "Farel",
+      "Age": 21,
+      "favColor": [
+        "Turqoise",
+        "Orange",
+        "Red",
+        "Blue",
+        "Purple",
+        "Turqoise",
+        "Orange",
+        "Red",
+        "Blue",
+        "Purple"
+      ]
+    },
+    {
+      "Name": "Lady",
+      "Age": 16,
+      "favColor": ["Green"]
+    },
+    {
+      "Name": "Falia",
+      "Age": 10,
+      "favColor": ["Red", "Blue", "Purple"]
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Dynamic App")
-        ,),
+        appBar: AppBar(title: Text("ID App")),
+        body: ListView(
+          children: myList.map((data) {
+            List myfavcolor = data["favColor"];
 
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Text(
-              counter.toString(),
-              style: TextStyle(fontSize: 50 + double.parse(counter.toString())),
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    if (counter > 1) {
-                      setState(() {
-                        counter--;  
-                      });
-                      
-                      print(counter);
-                    }
-                  },
-                  child: Icon(Icons.remove),
+            return Card(
+              margin: EdgeInsets.all(20),
+              color: Colors.grey[100],
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Name: ${data['Name']}"),
+                            Text("Age: ${data['Age']}"),
+                          ],
+                        )
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: myfavcolor.map((color) {
+                          return Container(
+                            child: Text(color),
+                            color: Colors.amber,
+                            margin:
+                                EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+                            padding: EdgeInsets.all(8),
+                          );
+                        }).toList(),
+                      ),
+                    ),
+                  ],
                 ),
-
-                ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      counter++;  
-                    });
-                    
-                    print(counter);
-                  },
-                  child: Icon(Icons.add),
-                ),
-              ],
-            )
-          ],
-        )
-      ,)
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
