bec4eab713269c847e74ae9294a064d621c6c302
alfarelzaki <alfarell.zaki@gmail.com>
2021-10-04 20:23:19 +0700

    text field decoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 main() {
   runApp(MyApp());
@@ -14,18 +15,46 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("TextField App"),
         ),
-
         body: Center(
           child: Padding(
             padding: const EdgeInsets.all(24.0),
             child: TextField(
-              autocorrect: true,
-              autofocus: true,
-              enableSuggestions: false,
-              enableInteractiveSelection: true,
+              showCursor: true,
+              cursorColor: Colors.green,
+              // cursorHeight: 24,
+              // cursorWidth: 12,
+              // cursorRadius: Radius.circular(20),
+
+              textAlign: TextAlign.start,
+              textAlignVertical: TextAlignVertical.center,
+              textCapitalization: TextCapitalization.none,
+
               obscureText: true,
-              obscuringCharacter: "#",
-              keyboardType: TextInputType.phone,
+
+              style: TextStyle(fontSize: 16, color: Colors.green),
+
+              decoration: InputDecoration(
+                icon: Icon(
+                  Icons.person,
+                  size: 24,
+                ),
+
+                border: OutlineInputBorder(),
+
+                prefixIcon: Icon(Icons.add_a_photo),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.remove_red_eye),
+                  onPressed: () {},
+                ),
+
+                // prefixText: "Name: ",
+
+                hintText: "Masukkan nama anda",
+                hintStyle: TextStyle(color: Colors.green, fontSize: 16),
+
+                labelText: "Nama lengkap",
+                labelStyle: TextStyle(color: Colors.black),
+              ),
             ),
           ),
         ),
