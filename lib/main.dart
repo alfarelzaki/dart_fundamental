c4943558910f4c53576a4d9465b0b9b4b4de4271
alfarelzaki <alfarell.zaki@gmail.com>
2021-10-05 17:12:35 +0700

    dialog popup

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
+import 'home.dart';
+
 main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  List<Container> myList = List.generate(60, (index) {
-    return Container(
-      height: 50,
-      width: 150,
-      color: Color.fromARGB(
-        255,
-        Random().nextInt(256),
-        Random().nextInt(256),
-        Random().nextInt(256)
-      ),
-    );
-  });
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text("Grid View")),
-      // body: GridView(
-      //   padding: EdgeInsets.all(16),
-      //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-      //     crossAxisCount: 3,
-      //     crossAxisSpacing: 16,
-      //     mainAxisSpacing: 16,
-      //     childAspectRatio: 1.0
-      //   ),
-      //   children: myList,
-      // ),
-
-      body: GridView.count(
-        padding: EdgeInsets.all(16),
-        crossAxisCount: 3,
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
-        childAspectRatio: 4/3,
-        children: myList,
-      ),
-    ));
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
   }
 }
+
