01d2aec103e01f5a062de7c6e46ede363b26494a
alfarelzaki <alfarell.zaki@gmail.com>
2021-10-09 13:52:05 +0700

    theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
-import 'package:new_flutter/pages/home_page.dart';
 
 main() {
   runApp(MyApp());
@@ -10,7 +9,76 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+        theme: ThemeData(
+          brightness: Brightness.light,
+          // visualDensity: VisualDensity.comfortable,
+          visualDensity: VisualDensity(horizontal: 3, vertical: 1),
+          primarySwatch: Colors.green,
+          textTheme: TextTheme(
+            bodyText2: TextStyle(
+              color: Colors.blue,
+              fontFamily: "RobotoMono",
+            ),
+          ),
+
+          appBarTheme: AppBarTheme(
+            color: Colors.red,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontFamily: "RobotoMono",
+            )
+          )
+        ));
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Theme",
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "this is a text",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            Text(
+              "this is a text",
+              style: TextStyle(
+                fontSize: 24,
+              ),
+            ),
+            SizedBox(
+              height: 24,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "Button",
+                style: TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton:
+          FloatingActionButton(onPressed: () {}, child: Icon(Icons.star)),
     );
   }
 }
--- lib/models/product.dart
+++
@@ -1,8 +0,0 @@
-class Product {
-  String judul;
-  String imageNetwork;
-  String harga;
-  String deskripsi;
-
-  Product(this.judul, this.imageNetwork, this.harga, this.deskripsi);
-}
