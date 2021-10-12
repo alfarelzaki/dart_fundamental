c7c6dd2ae4e3a9efd428845133bae7a05e4f17c3
alfarelzaki <alfarell.zaki@gmail.com>
2021-10-12 23:02:09 +0700

    setup state management project

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
-import 'dart:math';
-
-import 'package:faker/faker.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'dart:io';
+import './screens/products_overview_screen.dart';
+import './screens/product_detail_screen.dart';
 
-main() {
-  // make device orientation potrait only
-
-  // WidgetsFlutterBinding.ensureInitialized();
-  // SystemChrome.setPreferredOrientations([
-  //   DeviceOrientation.portraitUp,
-  // ]);
-
+void main() {
   runApp(MyApp());
 }
 
@@ -23,63 +12,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      title: 'MyShop',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        colorScheme: ColorScheme.fromSwatch(accentColor: Colors.amber),
+        fontFamily: 'Lato',
+      ),
+      home: ProductsOverviewScreen(),
+      routes: {
+        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+      },
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Cupertino design",
-          ),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () {
-              showDialog(
-                  context: context,
-                  builder: (context) {
-                    return Platform.isIOS
-                        ? CupertinoAlertDialog(
-                            title: Text("Delete item"),
-                            content: Text(
-                              "Are you sure you want to delete this item?",
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {},
-                                child: Text("No"),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text("Yes"),
-                              ),
-                            ],
-                          )
-                        : AlertDialog(
-                            title: Text("Delete item"),
-                            content: Text(
-                              "Are you sure you want to delete this item?",
-                            ),
-                            actions: [
-                              TextButton(
-                                onPressed: () {},
-                                child: Text("No"),
-                              ),
-                              TextButton(
-                                onPressed: () {},
-                                child: Text("Yes"),
-                              ),
-                            ],
-                          );
-                  });
-            },
-            child: Text("Alert dialog"),
-          ),
-        ));
   }
 }
