e76b9770664022b20668060b1b4e279ab45ce0f7
alfarelzaki <alfarell.zaki@gmail.com>
2021-11-04 13:45:56 +0700

    date picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:new_flutter/providers/all_products.dart';
-import 'package:provider/provider.dart';
-import './screens/products_overview_screen.dart';
-import './screens/product_detail_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,23 +8,75 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => Products(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'MyShop',
-        theme: ThemeData(
-          primarySwatch: Colors.green,
-          appBarTheme: AppBarTheme(
-            color: Colors.green,
-          ),
-          colorScheme: ColorScheme.fromSwatch(accentColor: Colors.amber),
-          fontFamily: 'Lato',
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Date picker",
         ),
-        home: ProductsOverviewScreen(),
-        routes: {
-          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-        },
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            selectedDate.toString(),
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 24),
+          Center(
+            child: OutlinedButton(
+              child: Text("Date picker"),
+              onPressed: () {
+                showDatePicker(
+                    context: context,
+                    initialDate: selectedDate,
+                    firstDate: DateTime(2000),
+                    lastDate: DateTime(2025),
+
+                    // initialDatePickerMode: DatePickerMode.year,
+                    // initialEntryMode: DatePickerEntryMode.input,
+
+                    selectableDayPredicate: (day) {
+                      if (day.isAfter(
+                              DateTime.now().subtract(Duration(days: 2))) &&
+                          day.isBefore(DateTime.now().add(Duration(days: 2)))) {
+                        return true;
+                      }
+                      return false;
+                    },
+                    helpText: "Help text",
+                    confirmText: "Confirm text",
+                    cancelText: "Cancel text",
+                    fieldHintText: "Field hint text",
+                    fieldLabelText: "Field label text",
+                    builder: (context, child) {
+                      return Theme(data: ThemeData.dark(), child: child!);
+                    }).then((value) {
+                  if (value != null) {
+                    setState(() {
+                      selectedDate = value;
+                    });
+                  }
+                });
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
